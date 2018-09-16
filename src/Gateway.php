e4b5209030e0663fa08ac76eac603cd966102954
Eric Chantigny <4933333+echantigny@users.noreply.github.com>
2018-09-16 15:42:30 -0400

    Adding get and set testMode.

--- src/Gateway.php
+++ src/Gateway.php
@@ -11,7 +11,6 @@ class Gateway extends AbstractGateway
     {
         return 'Moneris';
     }
-
     public function getDefaultParameters()
     {
         return array(
@@ -20,35 +19,36 @@ class Gateway extends AbstractGateway
             'testMode' => false
         );
     }
-
     public function getPsStoreId()
     {
         return $this->getParameter('psStoreId');
     }
-
     public function setPsStoreId($value)
     {
         return $this->setParameter('psStoreId', $value);
     }
-
     public function getHppKey()
     {
         return $this->getParameter('hppKey');
     }
-
     public function setHppKey($value)
     {
         return $this->setParameter('hppKey', $value);
     }
-
+    public function getTestMode()
+    {
+        return $this->getParameter('testMode');
+    }
+    public function setTestMode($value)
+    {
+        return $this->setParameter('testMode', $value);
+    }
     public function purchase(array $parameters = array())
     {
         return $this->createRequest('\Omnipay\Moneris\Message\PurchaseRequest', $parameters);
     }
-
     public function completePurchase(array $parameters = array())
     {
         return $this->createRequest('\Omnipay\Moneris\Message\CompletePurchaseRequest', $parameters);
     }
-
 }
