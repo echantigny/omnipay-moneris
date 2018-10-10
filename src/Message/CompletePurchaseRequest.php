edae163eea595ba0172fc7dd09ef09914f81bf1c
andris-sevcenko <asevcenko@gmail.com>
2018-10-10 10:42:12 +0300

    Allow the complete purchase request to hold actual response data.

--- src/Message/CompletePurchaseRequest.php
+++ src/Message/CompletePurchaseRequest.php
@@ -5,13 +5,17 @@ namespace Omnipay\Moneris\Message;
 /**
  * Moneris Complete Purchase Request
  */
-class CompletePurchaseRequest extends PurchaseRequest
+class CompletePurchaseRequest extends AbstractRequest
 {
     public function getData()
     {
-     	return $this->httpRequest->query->all();   
+        return $this->getParameter('response');
     }
 
+    public function setResponse($value)
+    {
+        return $this->setParameter('response', $value);
+    }
     public function sendData($data)
     {
         $this->response = new CompletePurchaseResponse($this, $data);
